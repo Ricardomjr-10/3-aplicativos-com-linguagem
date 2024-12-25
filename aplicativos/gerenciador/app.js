98f11979075044d96e8c5d1f56580c9d8fdfcef8
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-24 21:30:54 -0300

    build: function calcular despesas variaves

--- aplicativos/gerenciador/app.js
+++ aplicativos/gerenciador/app.js
@@ -1,7 +1,14 @@
 const buttonCalcFixas = document.querySelector('#calcular-fixas');
+const buttonCalcVariaveis = document.querySelector('#adicionar-despesa');
 const inputAluguel = document.querySelector('#aluguel');
 const inputContas = document.querySelector('#contas');
 const divTotalFixas = document.querySelector('#total-fixas');
+const inputAlimentacao = document.querySelector('#alimentacao');
+const inputTransporte = document.querySelector('#transporte');
+const divTotalVariaveis = document.querySelector('#total-variaveis');
+const ulDespesas = document.querySelector('#lista-despesas');
+const divEconomia = document.querySelector('#economia');
+const divDicas = document.querySelector('#dicas');
 
 buttonCalcFixas.addEventListener('click', () => {
     const aluguel = parseFloat(inputAluguel.value);
@@ -9,3 +16,10 @@ buttonCalcFixas.addEventListener('click', () => {
     const totalFixas = aluguel + contas;
     divTotalFixas.textContent = `Total Despesas Fixas: R$${totalFixas.toFixed(2)}`;
 });
+
+buttonCalcVariaveis.addEventListener('click', () => {
+    const alimentacao = parseFloat(inputAlimentacao.value)
+    const transporte = parseFloat(inputTransporte.value)
+    const totalVariaveis = alimentacao + transporte;
+    divTotalVariaveis.textContent = `Total Despesas Variáveis: R$${totalVariaveis.toFixed(2)}`
+})
